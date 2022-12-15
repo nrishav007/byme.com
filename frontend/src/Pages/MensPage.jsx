751ef01f567fb3ef5a3bb0b5047783185f7f19ef
Abrar Khan <heyabrarkhan@gmail.com>
2022-12-15 15:23:54 +0530

    Mens and Womens page modified

--- frontend/src/Pages/MensPage.jsx
+++ frontend/src/Pages/MensPage.jsx
@@ -1,5 +1,6 @@
-import { Box, Text } from "@chakra-ui/layout";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 
 export default function MensPage ( ) {
@@ -7,12 +8,17 @@ export default function MensPage ( ) {
         <>
         <Navbar/>
         <Box height={'85px'}></Box>
-        <Box borderBottom='1px solid black' bg='RGBA(0, 0, 0, 0.06)' fontSize={{base : '15px', md: '20px', lg :'25px'}} padding={{base : '10px', md :'15px'}} textAlign={{base :'center', md :'left'}} letterSpacing='2px' fontWeight='550'>
-            <Text>CLOTHING FOR MEN</Text>
-        </Box>
+        <Flex borderBottom='1px solid black' justifyContent='space-around' fontSize={{base : '10px', md: '15px', lg :'15px'}} padding={{base : '10px', md :'15px',lg :'10px'}} textAlign={{base :'center', md :'left'}}>
+            <Link to='/mens/allclothing'><Text _selected={{"color" : "red"}}>All Clothing</Text></Link>
+            <Link to='/mens/coats'><Text>Coats</Text></Link>
+            <Link to='/mens/dresses'><Text>Dresses</Text></Link>
+            <Link to='/mens/hosiery'><Text>Hosiery</Text></Link>
+            <Link to='/mens/jackets'><Text>Jackets</Text></Link>
+            <Link to='/mens/jeans'><Text>Jeans</Text></Link>
+        </Flex>
 
         <Box width={{base : "70%", md: '70%', lg : '60%'}} m='auto' mt={{base : '10px', md: '20px', lg: '30px'}}>
-            <Image m='auto' src='https://cdna.lystit.com/static/n/static-img/brand-ads/dolcegabbana-90263e5e-644d-4273-bea7-1eb0db2e5fa8.o.jpg'/>
+            <Image m='auto' src='https://cdna.lystit.com/static/n/static-img/brand-ads/stoneisland-d27ec2df-7ab3-4034-867c-6a239fca26cb.o.jpg'/>
         </Box>
         </>
     )
--- frontend/src/Pages/WomensPage.jsx
+++ frontend/src/Pages/WomensPage.jsx
@@ -1,8 +1,9 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import { Image } from "@chakra-ui/react";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 import {useSelector,useDispatch, shallowEqual} from "react-redux"
+import { Link } from "react-router-dom";
 
 export default function WomensPage ( ) {
     const {isLoading,isError,Womens} = useSelector((store) =>{
@@ -16,11 +17,16 @@ export default function WomensPage ( ) {
         <>
         <Navbar/>
         <Box height={'85px'}></Box>
-        <Box borderBottom='1px solid black' bg='RGBA(0, 0, 0, 0.06)' fontSize={{base : '15px', md: '20px', lg :'25px'}} padding={{base : '10px', md :'15px'}} textAlign={{base :'center', md :'left'}} letterSpacing='2px' fontWeight='550'>
-            <Text>CLOTHING FOR WOMEN</Text>
-        </Box>
+        <Flex borderBottom='1px solid black' justifyContent='space-around' fontSize={{base : '10px', md: '15px', lg :'15px'}} padding={{base : '10px', md :'15px',lg :'10px'}} textAlign={{base :'center', md :'left'}}>
+            <Link to='/womens/allclothing'><Text>All Clothing</Text></Link>
+            <Link to='/womens/coats'><Text>Coats</Text></Link>
+            <Link to='/womens/dresses'><Text>Dresses</Text></Link>
+            <Link to='/womens/hosiery'><Text>Hosiery</Text></Link>
+            <Link to='/womens/jackets'><Text>Jackets</Text></Link>
+            <Link to='/womens/jeans'><Text>Jeans</Text></Link>
+        </Flex>
 
-        <Box width={{base : "70%", md: '70%', lg : '60%'}} m='auto' mt={{base : '10px', md: '20px', lg: '30px'}}>
+        <Box width={{base : "80%", md: '70%', lg : '60%'}} m='auto' mt={{base : '10px', md: '20px', lg: '30px'}}>
             <Image m='auto' src='https://cdna.lystit.com/static/n/static-img/brand-ads/gucci-2b0c2682-47b4-49dc-bf75-abc3d285e122.o.jpg'/>
         </Box>
         </>
