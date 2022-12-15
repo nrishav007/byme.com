8dde8bc3b1bb4b97c13b9a36de5a2c6b91a1c160
Abrar Khan <heyabrarkhan@gmail.com>
2022-12-15 13:22:49 +0530

    Nav Modified

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,10 +1,16 @@
-import {Box,Flex,IconButton,useDisclosure,Stack,Text,Image,Input} from '@chakra-ui/react';
+import {Box,Flex,IconButton,useDisclosure,Stack,Text,Image,Input, Tooltip} from '@chakra-ui/react';
 import {Link} from "react-router-dom"
 import { HamburgerIcon, CloseIcon} from '@chakra-ui/icons';
 import {BsSearch} from "react-icons/bs"
+import { useContext } from 'react';
+import { AuthContext } from '../context/AppContext';
+import {FaUserCircle} from "react-icons/fa"
+import {BiLogIn} from "react-icons/bi"
+import {AiOutlineHeart} from "react-icons/ai"
 
 export default function Navbar( ) {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const {user} = useContext(AuthContext);
     return (
       <>
         <Box px={4}  className='ChackraNavBar' shadow='lg' borderBottom={{base : '0px', md :'1px solid black'}} padding='10px'>
@@ -27,11 +33,11 @@ export default function Navbar( ) {
                 <Input borderRadius='0px' border='0px' placeholder='SEARCH' variant="unstyled" padding='5px'/>
                </Flex>
 
-               <Flex width={{base : "", md  : '20%', lg :'10%'}} justifyContent='space-between'>
-               <Link to="/signup"><Text _hover={{borderBottom : "1px solid black", fontWeight : "550"}}>SIGN UP</Text></Link>
-               <Link to="/login"><Text _hover={{borderBottom : "1px solid black", fontWeight : "550"}}>LOGIN</Text></Link>
+               <Flex width={{base : "", md  : '20%', lg :'10%'}} justifyContent='space-around' fontSize='25px'>
+               <Tooltip bg='#CBD5E0' color='black' label='Sing Up'><Link to="/signup"><Text><BiLogIn/></Text></Link></Tooltip>
+               <Tooltip bg='#CBD5E0' color='black' label='Wishlist'><Link to="/wishlist"><Text><AiOutlineHeart/></Text></Link></Tooltip>
+              <Tooltip bg='#CBD5E0' color='black' label={user.displayName ? user.displayName : 'Profile'}><Link to={user.displayName ? '/' : '/login'}><Text><FaUserCircle/></Text></Link></Tooltip>
               </Flex>
-
               </Flex>
           </Flex> 
           {isOpen ? (
@@ -39,6 +45,7 @@ export default function Navbar( ) {
               <Stack as={'nav'} spacing={4}>
                   <Link to='/men'><Text>MEN</Text></Link>
                   <Link to='/women'><Text>WOMEN</Text></Link>
+                  <Link to='/wishlist'><Text>WISHLIST</Text></Link>
                   <Link to='/sginup'><Text>SIGN UP</Text></Link>
                   <Link to='/login'><Text>LOG IN</Text></Link>
               </Stack>
--- frontend/src/Pages/MensPage.jsx
+++ frontend/src/Pages/MensPage.jsx
@@ -1,4 +1,5 @@
 import { Box, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import Navbar from "../Components/Navbar";
 
 export default function MensPage ( ) {
@@ -8,6 +9,10 @@ export default function MensPage ( ) {
         <Box height={'85px'}></Box>
         <Box borderBottom='1px solid black' bg='RGBA(0, 0, 0, 0.06)' fontSize={{base : '15px', md: '20px', lg :'25px'}} padding={{base : '10px', md :'15px'}} textAlign={{base :'center', md :'left'}} letterSpacing='2px' fontWeight='550'>
             <Text>CLOTHING FOR MEN</Text>
+        </Box>
+
+        <Box width={{base : "70%", md: '70%', lg : '60%'}} m='auto' mt={{base : '10px', md: '20px', lg: '30px'}}>
+            <Image m='auto' src='https://cdna.lystit.com/static/n/static-img/brand-ads/dolcegabbana-90263e5e-644d-4273-bea7-1eb0db2e5fa8.o.jpg'/>
         </Box>
         </>
     )
