2d5287f46e6503f7547cc7c659adf0dc5b3a51eb
Ashutosh5333 <lakshakarashutosh@gmail.com>
2022-12-17 23:44:38 +0530

    imagemangiferdone

--- frontend/src/CustomComponents/ProductsListing.jsx
+++ frontend/src/CustomComponents/ProductsListing.jsx
@@ -8,21 +8,8 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { AddToWishListData } from "../Fetch/Fetch";
 
 
-    // const handleAddToWish = (image,title,price,category,description,type,_id) =>{
-    //     const payload = {
-    //         productID : _id,
-    //         image,
-    //         title,
-    //         price,
-    //         category,
-    //         description,
-    //         type
-    //     }
-    //     return AddToWishListData(payload).then((res)=>{
-    //         Toast({position : "top", title : `${res.data.msg}`, status : res.data.msg === 'Product already in wishlist' ? 'error' : 'success',duration : 3000})
-    //     })
-    //     .catch((err)=> console.log(err))
-    // }
+   
+
 export default function ProductsListing({ data, isLoading, isError }) {
   const Toast = useToast();
 
@@ -50,44 +37,6 @@ export default function ProductsListing({ data, isLoading, isError }) {
       })
       .catch((err) => console.log(err));
   };
-
-  return (
-    <>
-      {isLoading && <ProgressLoader size="sm" colorScheme="black" />}
-      <Text textAlign="center">
-        {isError && "Oops!! Something  Went Wrong"}
-      </Text>
-      <SimpleGrid
-        columns={[2, 2, 3, 6]}
-        width={{ base: "95%" }}
-        m="auto"
-        rowGap={{ base: "20px", lg: "50px" }}
-        columnGap={{ base: "20px", lg: "30px" }}
-      >
-        {data.length > 0 &&
-          data.map((elem) => {
-            return (
-              <Box padding="10px" lineHeight={{ base: "30px" }} key={elem._id}>
-                <Text
-                  fontSize={{ base: "14px", md: "15px" }}
-                  fontWeight="550"
-                  align="center"
-                >
-                  Title : {elem.title}
-                </Text>
-                <Text
-                  fontSize={{ base: "14px", md: "15px" }}
-                  fontWeight="550"
-                  align="center"
-                >
-                  Price : {elem.price}
-                </Text>
-              </Box>
-            );
-          })}
-      </SimpleGrid>
-    </>
-  );
     return (
         <>
         {isLoading && <ProgressLoader size='sm'  colorScheme='black'/>}
