70568196228a3dbf1ddb780b2c984a97e19c36e9
Ashutosh5333 <lakshakarashutosh@gmail.com>
2022-12-18 14:04:25 +0530

    helpbot is ready

--- frontend/src/Pages/Helpbot/Helpbot.jsx
+++ frontend/src/Pages/Helpbot/Helpbot.jsx
@@ -22,7 +22,6 @@ export class Helpbot extends Component {
   }
 
 
-
   render() {
     return (
       <div>
--- frontend/src/Pages/Singlepage/single.js
+++ frontend/src/Pages/Singlepage/single.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from '../../Components/Footer'
 import { Helpbot } from '../Helpbot/Helpbot'
 import Details from './Details'
-import "./Sin.css"
+
 
 export const Single = () => {
   return (
