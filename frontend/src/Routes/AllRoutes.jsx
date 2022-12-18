3ef98e31baa613af07e3863da2d5d477bdeb08cc
Abrar Khan <heyabrarkhan@gmail.com>
2022-12-18 21:57:30 +0530

    Final AllSet

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router";
 import { Admin } from "../admin/Admin";
-import AdminProducts from "../admin/Products";
 import MensBeachWear from "../Components/MensBeach";
 import MensCoatsPage from "../Components/MensCoat";
 import MensJacketsPage from "../Components/MensJackets";
@@ -34,25 +33,25 @@ export default function AllRoutes ( ) {
             <Route path="/signup" element={<Signup/>}/>
             <Route path="/login" element={<Login/>}/>
             <Route path="/women/all-clothing" element={<PrivateRoute><WomensPage/></PrivateRoute>}/>
-            <Route path="/women/coats" element={<WomenCoatsPage/>}/>
-            <Route path="/women/beach-wear" element={<WomenSwimWear/>}/>
-            <Route path="/women/hosiery" element={<WomenHosiery/>}/>
-            <Route path="/women/jackets" element={<WomensJackets/>}/>
-            <Route path="/women/jeans" element={<WomensJeansPage/>}/>
-            <Route path="/men/all-clothing" element={<MensPage/>}/>
-            <Route path="/men/coats" element={<MensCoatsPage/>}/>
-            <Route path="/men/beach-wear" element={<MensBeachWear/>}/>
-            <Route path="/men/sweaters" element={<MensSweaters/>}/>
-            <Route path="/men/jackets" element={<MensJacketsPage/>}/>
-            <Route path="/men/jeans" element={<MensJeansPage/>}/>
-            <Route path="/product/single/:id" element={<Single/>}/>
-            <Route path="/wishlist" element={<WishlistPage/>}/>
+            <Route path="/women/coats" element={<PrivateRoute><WomenCoatsPage/></PrivateRoute>}/>
+            <Route path="/women/beach-wear" element={<PrivateRoute><WomenSwimWear/></PrivateRoute>}/>
+            <Route path="/women/hosiery" element={<PrivateRoute><WomenHosiery/></PrivateRoute>}/>
+            <Route path="/women/jackets" element={<PrivateRoute><WomensJackets/></PrivateRoute>}/>
+            <Route path="/women/jeans" element={<PrivateRoute><WomensJeansPage/></PrivateRoute>}/>
+            <Route path="/men/all-clothing" element={<PrivateRoute><MensPage/></PrivateRoute>}/>
+            <Route path="/men/coats" element={<PrivateRoute><MensCoatsPage/></PrivateRoute>}/>
+            <Route path="/men/beach-wear" element={<PrivateRoute><MensBeachWear/></PrivateRoute>}/>
+            <Route path="/men/sweaters" element={<PrivateRoute><MensSweaters/></PrivateRoute>}/>
+            <Route path="/men/jackets" element={<PrivateRoute><MensJacketsPage/></PrivateRoute>}/>
+            <Route path="/men/jeans" element={<PrivateRoute><MensJeansPage/></PrivateRoute>}/>
+            <Route path="/product/single/:id" element={<PrivateRoute><Single/></PrivateRoute>}/>
+            <Route path="/wishlist" element={<PrivateRoute><WishlistPage/></PrivateRoute>}/>
             <Route path="*" element={<NotFoundPage/>}/>
-            <Route path="/cart" element={<CartPage/>}/>
-            <Route path="/userdetails" element={<DummyData/>}/>
-            <Route path="/payment" element={<PaymentPage/>}/>
-            <Route path="/checkout" element={<CheckoutPage/>}/>
-            <Route path="/admin" element={<Admin/>} />
+            <Route path="/cart" element={<PrivateRoute><CartPage/></PrivateRoute>}/>
+            <Route path="/userdetails" element={<PrivateRoute><DummyData/></PrivateRoute>}/>
+            <Route path="/payment" element={<PrivateRoute><PaymentPage/></PrivateRoute>}/>
+            <Route path="/checkout" element={<PrivateRoute><CheckoutPage/></PrivateRoute>}/>
+            <Route path="/admin" element={<PrivateRoute><Admin/></PrivateRoute>} />
         </Routes>
         </> 
     )
